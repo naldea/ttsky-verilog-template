cd064c09ae37323ee09a96a99c6f17a4a86f8f5d
Naomi Aldea Zambrano <naomialdeazambrano@MacBook-Air-de-Naomi.local>
2025-09-07 23:34:01 -0300

    prueba1 semaforo

--- test/tb.v
+++ test/tb.v
@@ -4,55 +4,55 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tb_semaforo;
+module tb ();
 
-    logic clk, rst;
-    logic TA, TB, P, R;
-    wire [1:0] LA, LB;
-    wire on;
+  // Dump the signals to a VCD file. You can view it with gtkwave.
+  initial begin
+    $dumpfile("tb.vcd");
+    $dumpvars(0, tb);
+    #1;
+  end
 
-    // Instancia del DUT (Device Under Test)
-    tt_um_semaforo uut (
-        .ui_in  ({4'b0000, R, P, TB, TA}), // mapeo: [3]=R, [2]=P, [1]=TB, [0]=TA
-        .uo_out (),                        // conectaremos abajo con LA, LB, on
-        .uio_in (8'b0),
-        .uio_out(),
-        .uio_oe (),
-        .ena    (1'b1),    // enable siempre activo
-        .clk    (clk),
-        .rst_n  (~rst)
-    );
+  // Wire up the inputs and outputs:
+  reg clk;
+  reg rst_n;
+  reg ena;
+  wire [7:0] ui_in;
+  wire [7:0] uio_in;
+  reg [7:0] ui_in_reg;
+  reg [7:0] uio_in_reg;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
-    // Extraemos las señales desde uo_out
-    wire [7:0] uo_out = uut.uo_out;
-    assign LA = uo_out[1:0];
-    assign LB = uo_out[3:2];
-    assign on = uo_out[4];
+  // Replace tt_um_example with your module name:
+  tt_um_semaforo user_project (
 
-    // Generador de reloj
-    initial clk = 0;
-    always #5 clk = ~clk;
+      // Include power ports for the Gate Level test:
+`ifdef GL_TEST
+      .VPWR(1'b1),
+      .VGND(1'b0),
+`endif
 
-    // Estímulos
-    initial begin
-        rst = 1; TA=0; TB=0; P=0; R=0;
-        #20 rst=0;
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
+  );
 
-        TA=1; TB=0; #100;
-        TA=0; #100;
-        TB=1; #100;
-        TB=0; #100;
 
-        P=1; #10; P=0; #50;
-        R=1; #10; R=0;
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;  
+  end
 
-        #200 $finish;
-    end
-
-    // Dump VCD
-    initial begin
-        $dumpfile("semaforo.vcd");
-        $dumpvars(0, tb_semaforo);
-    end
+  initial begin
+    rst_n = 0;  
+    $stop;
+  end
 
 endmodule
