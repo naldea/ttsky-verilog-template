cef8529c3c2d83f4c709dda9232c55c55f27166b
Naomi Aldea Zambrano <naomialdeazambrano@MacBook-Air-de-Naomi.local>
2025-09-07 23:48:10 -0300

    prueba1 semaforo

--- test/tb.v
+++ test/tb.v
@@ -6,7 +6,7 @@
 */
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave.
+  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
@@ -17,21 +17,23 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  wire [7:0] ui_in;
-  wire [7:0] uio_in;
-  reg [7:0] ui_in_reg;
-  reg [7:0] uio_in_reg;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+`ifdef GL_TEST
+  wire VPWR = 1'b1;
+  wire VGND = 1'b0;
+`endif
 
   // Replace tt_um_example with your module name:
   tt_um_semaforo user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
+      .VPWR(VPWR),
+      .VGND(VGND),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
@@ -43,16 +45,5 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
-
-
-  initial begin
-    clk = 0;
-    forever #5 clk = ~clk;  
-  end
-
-  initial begin
-    rst_n = 0;  
-    $stop;
-  end
 
 endmodule
